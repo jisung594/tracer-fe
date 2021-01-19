d1eb5b9bcf28abdb41fd9ccab46830818ff3d2b0
Jonathan Choi <jisung594@gmail.com>
2021-01-19 13:40:19 -0500

    project structure

---
+++ src/Components/AssetProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AssetProfile = () => {
+
+  // Display candlestick chart and basic info of company, high/low's, etc.
+
+  return (
+
+  )
+}
+
+export default AssetProfile;
---
+++ src/Components/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index = () => {
+
+  // Display all items based on selection of Stocks, Crypto, or Forex
+
+  return (
+
+  )
+}
+
+export default Index;
