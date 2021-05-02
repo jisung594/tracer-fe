9fd91153cb08d0492f32f51b63efae89a92167a7
Jonathan Choi <jisung594@gmail.com>
2021-05-02 12:43:44 -0500

    test

--- src/Components/SecurityIndex.js
+++ src/Components/SecurityIndex.js
@@ -20,7 +20,13 @@ const SecurityIndex = () => {
 
   useEffect(() => {
     // set env variable for token in backend and pass in other param's ('stock','exchange','limit',etc.)
-    fetch('https://tracerscfx-server.herokuapp.com/stocks_us')
+    fetch('https://tracerscfx-server.herokuapp.com/stocks_us', {
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin':'*',
+        'Content-Type': 'application/json'
+      }
+    })
     // fetch('http://127.0.0.1:5000/stocks_us')
       .then(res => res.json())
       .then(stocks => {
